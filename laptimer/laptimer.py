7ba5e72e717a5cbd70353039266783a31b5ac8df
javirugo <jruizmail@gmail.com>
2016-05-06 00:05:51 +0200

    added some initial missing things...

--- laptimer/laptimer.py
+++ laptimer/laptimer.py
@@ -28,7 +28,6 @@ SECTOR3_POLY = Polygon([
     (track_data["SECTOR3"]["lat3"], track_data["SECTOR3"]["lon3"])
 ])
 
-LAPS = []
 metricsSource = Metrics()
 metricsSource.setMockData(track_data["MOCK"])
 
@@ -36,22 +35,20 @@ current_sector = 0
 current_lap = 0
 inside_poly = False
 
-LAPS.append({ "started": False, "ended": False,
+LAPS = [{ "started": False, "ended": False,
 	  "sectors": [
 	    {"started": False, "ended": False},
 	    {"started": False, "ended": False},
-	    {"started": False, "ended": False} ]})
+	    {"started": False, "ended": False} ]}]
 while True:
     cur_ts, cur_lat, cur_lon = metricsSource.getMetrics()
     current_point = Point(cur_lat, cur_lon)
-    # print "%s, %s, %s" % (cur_ts, cur_lat, cur_lon)
 
     if current_point.within(START_POLY):
         if inside_poly:
             continue
         else:
             inside_poly = True
-	    print "start!"
             LAPS[current_lap]["ended"] = cur_ts
             LAPS[current_lap]["sectors"][2]["ended"] = cur_ts
 
@@ -71,7 +68,6 @@ while True:
         if inside_poly:
             continue
         else:
-	    print "SECTOR2"
             inside_poly = True
 
             LAPS[current_lap]["sectors"][0]["ended"] = cur_ts
@@ -81,7 +77,6 @@ while True:
         if inside_poly:
             continue
         else:
-	    print "SECTOR3"
             inside_poly = True
 
             LAPS[current_lap]["sectors"][1]["ended"] = cur_ts
@@ -89,8 +84,16 @@ while True:
 
     else:
         inside_poly = False
-    
-    if len(LAPS) > 2:
+
+    if len(LAPS) > 10:
       break
 
-print LAPS
+lap_no = 1
+for lap in LAPS:
+  if not lap["started"] or not lap["ended"]: continue
+
+  print "Lap %i: %s" % (lap_no, round((lap["ended"] - lap["started"]).total_seconds(), 3))
+  print "\tsec1: %s secs" % round((lap["sectors"][0]["ended"] - lap["sectors"][0]["started"]).total_seconds(), 3)
+  print "\tsec2: %s secs" % round((lap["sectors"][1]["ended"] - lap["sectors"][1]["started"]).total_seconds(), 3)
+  print "\tsec3: %s secs" % round((lap["sectors"][2]["ended"] - lap["sectors"][2]["started"]).total_seconds(), 3)
+  lap_no += 1
--- laptimer/mock_metrics.py
+++ laptimer/mock_metrics.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from datetime import datetime
+import time
+import random
 
 class Metrics:
 
@@ -9,6 +11,7 @@ class Metrics:
     self.index = 0
 
   def getMetrics(self):
+    time.sleep(round(random.uniform(0.001, 0.090), 3))
     if self.index == (len(self.data) -1):
       self.index = 0
 
